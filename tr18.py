0d963cc9ef5310d8768d45c2d526e56a50eb7432
Josyula Sita sai sudha <114298645+sitasaisudha@users.noreply.github.com>
2023-12-01 22:36:36 +0530

    Update tr18.py

--- tr18.py
+++ tr18.py
@@ -48,7 +48,7 @@ def lemmatizer(text):
     return ' '.join(sent)
 
 st.title("Sentiment Analysis App")
-st.markdown("-By Sita sai sudha")
+st.markdown("-By Uday kiran Medicherla")
 image = Image.open("sentiment.jpeg")
 st.image(image, use_column_width=True)
 
